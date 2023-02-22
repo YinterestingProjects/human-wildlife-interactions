282e55268c76acc2221cc0d8fe2c858bfc64e3e4
Sally Yin <100384553+YinterestingProjects@users.noreply.github.com>
2023-02-21 22:18:42 -0600

    adding script to transform api data
    - combine.py transforms all api outputs in directory into a video details df, video comments df, and a failed list for videos that had invalid errors from the YT API
    - updated api_consolidation notebook (sandbox work for combine.py)
    - new failed_id list update

---
+++ src/data/combine.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import json
+import os
+from tqdm import tqdm
+import pickle
+
+
+def get_videoDetails(data, report = False):
+    '''return a dataframe of all video details features from Youtube API and a set of failed ids'''
+
+    buffer = []
+    failed_ids = set()
+    for idx in data.keys():
+        if data[idx]['details'] == 'Invalid lookup.':
+            failed_ids.add(idx) 
+        elif len(data[idx]['details']['items']) == 0:
+            failed_ids.add(idx)
+        else:
+            data[idx]['details']['items'][0]['yt8M_id'] = idx
+            buffer.append(data[idx]['details']['items'][0])
+    
+    videoDets_df = pd.json_normalize(buffer, errors='ignore')
+    
+    if report:
+        print(f'n of videos successful: {videoDets_df.shape[0]}')
+        print(f'n of videos failed: {len(failed_ids)}')
+    
+    return videoDets_df, failed_ids
+    
+
+def get_videoComments(data, report=False):
+    '''return a dataframe of all comments features from Youtube API'''
+    
+    buffer = []
+    no_comment_count = 0
+    for idx in data.keys():
+        if data[idx]['comments'] == 'Invalid lookup':
+            no_comment_count += 1
+        elif len(data[idx]['comments']['items']) == 0:
+            no_comment_count += 1
+        else:
+            data[idx]['comments']['items'][0]['yt8M_id'] = idx
+            buffer.append(data[idx]['comments']['items'][0])
+    
+    videoComs_df = pd.json_normalize(buffer, errors='ignore')
+    
+    if report:
+        print(f'{no_comment_count = }')
+
+    return videoComs_df
+
+
+def combine(files_to_combine):
+    failed_ids = set()
+    vDets_buffer = []
+    vComs_buffer = []
+    
+    for file in tqdm(files_to_combine):
+        with open(file) as file:
+            f = json.load(file)
+        
+        vDets_df, fail_ids = get_videoDetails(f, report=True)
+        vDets_buffer.append(vDets_df)
+        failed_ids.update(fail_ids)
+        
+        vComs_df = get_videoComments(f, report=True)
+        vComs_buffer.append(vComs_df)
+        
+    
+    vDets = pd.concat(vDets_buffer,ignore_index=True)
+    vComs = pd.concat(vComs_buffer,ignore_index=True)
+    
+    return vDets, vComs, failed_ids
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_dir', help='directory of JSON files (txt)')
+    parser.add_argument('output_dir', help='directory to output combined videoDetails pickled dataframe (pkl), videoComments pickled dataframe (pkl), and set of failed ids (pkl)')
+    args = parser.parse_args()
+
+    input_files = [os.path.join(args.input_dir, f) 
+        for f in sorted(os.listdir(args.input_dir))]
+
+    videoDets, videoComs, failed_ids = combine(input_files)
+    
+    with open(os.path.join(args.output_dir, 'videoDets.pkl'), 'wb') as f:
+        pickle.dump(videoDets, f)
+    
+    with open(os.path.join(args.output_dir, 'videoComs.pkl'), 'wb') as f:
+        pickle.dump(videoComs, f)
+
+    with open(os.path.join(args.output_dir, 'failed_ids.pkl'), 'wb') as f:
+        pickle.dump(failed_ids, f)
